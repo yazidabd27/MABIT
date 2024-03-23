4588dc76d70d522c97a28e229058883078674663
yazid <yazidabd35@gmail.com>
2024-03-24 00:32:01 +0100

    first upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('#hero img');
+    let currentImg = 0;
+  
+    setInterval(() => {
+      images[currentImg].classList.remove('active');
+      currentImg = (currentImg + 1) % images.length;
+      images[currentImg].classList.add('active');
+    }, 3000);
+});
+
+var about=document.querySelector('#about');
+var prg=document.querySelector('#about .about-description');
+var services=document.querySelector('#services');
+var cards=document.querySelectorAll('.cards .card');
+
+window.onscroll=(()=>{
+  if(window.scrollY>=about.offsetTop - 450){
+    prg.classList.add('visible');
+  }
+  
+  if(window.scrollY>=services.offsetTop - 500){
+    cards.forEach((card, index)=>{
+      setTimeout(()=>{
+        card.classList.add('visible');
+      }, index*750)
+    })
+  }
+})
